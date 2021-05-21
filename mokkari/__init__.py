d1cce0088cbc6679f0610943b9b5493b7462f4ab
Brian Pepple <bdpepple@gmail.com>
2021-05-21 15:32:50 -0400

    Add docstring for api()

--- mokkari/__init__.py
+++ mokkari/__init__.py
@@ -1,11 +1,16 @@
 from . import exceptions, sesssion
 
-"""Projects version information used in setup.py"""
+# Projects version information used in setup.py
 VERSION_INFO = (0, 0, 2)
 VERSION = ".".join(str(c) for c in VERSION_INFO)
 
 
 def api(username=None, passwd=None):
+    """ Entry function the sets login credentials for metron.cloud.
+
+    :param username: The username used for metron.cloud.
+    :param passwd: The password used for metron.cloud.
+    """
     if username is None:
         raise exceptions.AuthenticationError("Missing username.")
 
