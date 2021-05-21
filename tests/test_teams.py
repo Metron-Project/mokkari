168a65dcdf29094d8fa82e92d742881f21633618
Brian Pepple <bdpepple@gmail.com>
2021-05-20 23:00:09 -0400

    Add test for Team api errors

--- tests/test_teams.py
+++ tests/test_teams.py
@@ -1,7 +1,8 @@
 import os
 import unittest
 
-import mokkari
+from mokkari import api, exceptions
+from mokkari.teams_list import TeamsList
 
 # TODO: Should mock the responses but for now let's use live data
 
@@ -10,7 +11,7 @@ class TestTeams(unittest.TestCase):
     def setUp(self):
         username = os.getenv("METRON_USERNAME", "username")
         passwd = os.getenv("METRON_PASSWD", "passwd")
-        self.c = mokkari.api(username=username, passwd=passwd)
+        self.c = api(username=username, passwd=passwd)
 
     def test_known_team(self):
         inhumans = self.c.team(1)
@@ -25,6 +26,14 @@ class TestTeams(unittest.TestCase):
     def test_teamlist(self):
         teams = self.c.teams_list()
         self.assertGreater(len(teams.teams), 0)
+
+    def test_bad_team(self):
+        with self.assertRaises(exceptions.ApiError):
+            self.c.team(-1)
+
+    def test_bad_response_data(self):
+        with self.assertRaises(exceptions.ApiError):
+            TeamsList({"results": {"name": 1}})
 
 
 if __name__ == "__main__":
