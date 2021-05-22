80924158cd42a6bef0a421de373cdc6bc6ad7266
Brian Pepple <bdpepple@gmail.com>
2021-05-22 16:55:34 -0400

    Bump version for development

--- mokkari/__init__.py
+++ mokkari/__init__.py
@@ -1,7 +1,7 @@
 from . import exceptions, sesssion
 
 # Projects version information used in setup.py
-VERSION_INFO = (0, 0, 4)
+VERSION_INFO = (0, 1, 0)
 VERSION = ".".join(str(c) for c in VERSION_INFO)
 
 
