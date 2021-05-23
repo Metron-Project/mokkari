49e3aa5b7b8353fea9648f90601c103dc7bed462
Brian Pepple <bdpepple@gmail.com>
2021-05-23 10:40:17 -0400

    Update version for docs & sonarcloud

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Brian Pepple'
 author = 'Brian Pepple'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.4'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
