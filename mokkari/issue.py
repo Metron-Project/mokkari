e4a2b00e8a3fd27d13196a517147a6807e0118bb
Brian Pepple <bdpepple@gmail.com>
2021-06-17 00:17:34 -0400

    Mark api changes in documentation
    Use schema field data_key param for more precise naming of field

--- mokkari/issue.py
+++ mokkari/issue.py
@@ -57,14 +57,23 @@ class Issue:
 
 
 class IssueSchema(Schema):
-    """ Schema for the Issue API."""
+    """
+    Schema for the Issue API.
+
+    .. versionchanged:: 0.1.6
+        ``name`` field  changed to ``story_titles``
+
+    .. versionchanged:: 0.1.6
+        ``__str__`` field change to ``issue_name``
+
+    """
 
     id = fields.Int()
     publisher = fields.Nested(publisher.PublisherSchema)
     series = fields.Nested(series.SeriesSchema)
     volume = fields.Int()
     number = fields.Str()
-    name = fields.List(fields.Str(allow_none=True), attribute="story_titles")
+    story_titles = fields.List(fields.Str(allow_none=True), data_key="name")
     cover_date = fields.Date()
     store_date = fields.Date(allow_none=True)
     desc = fields.Str(allow_none=True)
@@ -73,7 +82,7 @@ class IssueSchema(Schema):
     credits = fields.Nested(CreditsSchema, many=True)
     characters = fields.Nested(character.CharacterSchema, many=True)
     teams = fields.Nested(team.TeamSchema, many=True)
-    __str__ = fields.Str(attribute="issue_name")
+    issue_name = fields.Str(data_key="__str__")
 
     class Meta:
         unknown = INCLUDE
