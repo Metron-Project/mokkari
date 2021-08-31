3b56ff8c8aea0f38338f7fecb1d85094f0f86583
Brian Pepple <bdpepple@gmail.com>
2021-08-31 08:59:15 -0400

    Add missing docstrings

--- mokkari/team.py
+++ mokkari/team.py
@@ -1,10 +1,25 @@
+"""
+Team module.
+
+This module provides the following classes:
+
+- Team
+- TeamSchema
+"""
 from marshmallow import INCLUDE, Schema, fields, post_load
 
 from mokkari import creator
 
 
 class Team:
+    """
+    The Team object contains information for teams.
+
+    :param `**kwargs`: The keyword arguments is used for setting team data from Metron.
+    """
+
     def __init__(self, **kwargs):
+        """Intialize a new Team."""
         for k, v in kwargs.items():
             setattr(self, k, v)
 
@@ -20,6 +35,8 @@ class TeamSchema(Schema):
     creators = fields.Nested(creator.CreatorSchema, many=True)
 
     class Meta:
+        """Any unknown fields will be included."""
+
         unknown = INCLUDE
 
     @post_load
