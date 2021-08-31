532d13ad58391a5e47ebfc341c560bd2fc11183a
Brian Pepple <bdpepple@gmail.com>
2021-08-31 09:18:44 -0400

    Add missing docstrings

--- mokkari/exceptions.py
+++ mokkari/exceptions.py
@@ -1,13 +1,33 @@
+"""
+Exceptions module.
+
+This module provides the following classes:
+
+- ApiError
+- AuthenticationError
+- CacheError
+"""
+
+
 class ApiError(Exception):
+    """Class for any api errors."""
+
     def __init__(self, *args, **kwargs):
+        """Initialize an ApiError."""
         Exception.__init__(self, *args, **kwargs)
 
 
 class AuthenticationError(ApiError):
+    """Class for any authentication errors."""
+
     def __init__(self, *args, **kwargs):
+        """Initialize an AuthenticationError."""
         Exception.__init__(self, *args, **kwargs)
 
 
 class CacheError(ApiError):
+    """Class for any database cache errors."""
+
     def __init__(self, *args, **kwargs):
+        """Initialize an CacheError."""
         Exception.__init__(self, *args, **kwargs)
--- mokkari/variant.py
+++ mokkari/variant.py
@@ -1,8 +1,23 @@
+"""
+Variant module.
+
+This module provides the following classes:
+
+- Variant
+- VariantSchema
+"""
 from marshmallow import INCLUDE, Schema, fields, post_load
 
 
 class Variant:
+    """
+    The Variant object contains information for variant issues.
+
+    :param `**kwargs`: The keyword arguments is used for setting variant issue data.
+    """
+
     def __init__(self, **kwargs):
+        """Intialize a new Variant."""
         for k, v in kwargs.items():
             setattr(self, k, v)
 
@@ -15,6 +30,8 @@ class VariantSchema(Schema):
     image = fields.Url()
 
     class Meta:
+        """Any unknown fields will be included."""
+
         unknown = INCLUDE
 
     @post_load
