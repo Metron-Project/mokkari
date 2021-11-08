ac7f14b30b7ce32fe829ddfa739242c0537f9977
Brian Pepple <bdpepple@gmail.com>
2021-11-08 10:42:58 -0500

    Add docstrings to api()

--- mokkari/__init__.py
+++ mokkari/__init__.py
@@ -12,12 +12,17 @@ def api(
     username: Optional[str] = None,
     passwd: Optional[str] = None,
     cache: sqlite_cache.SqliteCache = None,
-):
+) -> session.Session:
     """Entry function the sets login credentials for metron.cloud.
 
     :param str username: The username used for metron.cloud.
     :param str passwd: The password used for metron.cloud.
     :param SqliteCache optional: SqliteCache to use
+
+    :return: :class:`Session`
+    :rtype: Session
+
+    :raises: :class:`AuthenticationError`
     """
     if username is None:
         raise exceptions.AuthenticationError("Missing username.")
