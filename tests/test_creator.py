1a33ef70af2adad6423df9e32c44b07d9f07711b
Brian Pepple <bdpepple@gmail.com>
2021-11-08 12:24:21 -0500

    Add test for creator validation failure

--- tests/test_creator.py
+++ tests/test_creator.py
@@ -3,6 +3,7 @@ Test Creator module.
 
 This module contains tests for Creator objects.
 """
+import json
 from datetime import date, datetime, timedelta, timezone
 
 import pytest
@@ -61,3 +62,27 @@ def test_bad_response_data(talker):
     """Test for a bad creator response."""
     with pytest.raises(exceptions.ApiError):
         creator.CreatorsList({"results": {"name": 1}})
+
+
+def test_bad_creator_validate(talker):
+    """Test data with invalid data."""
+    # Change the 'name' field to an int, when it should be a string.
+    data = {
+        "id": 150,
+        "name": 50,
+        "birth": None,
+        "death": None,
+        "desc": "Foo Bar",
+        "wikipedia": "Jim_Cheung",
+        "image": "https://static.metron.cloud/media/creator/2018/11/13/Jim_Cheung.jpg",
+        "modified": "2019-06-23T15:13:22.423371-04:00",
+    }
+
+    with requests_mock.Mocker() as r:
+        r.get(
+            "https://metron.cloud/api/creator/150/",
+            text=json.dumps(data),
+        )
+
+        with pytest.raises(exceptions.ApiError):
+            talker.creator(150)
