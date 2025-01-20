13545a3580b6a0d575ab2e01a48ce56d1e41ba1e
Brian Pepple <bdpepple@gmail.com>
2025-01-20 14:59:52 -0500

    Bump tp 3.6.0 (#74)

--- mokkari/__init__.py
+++ mokkari/__init__.py
@@ -1,7 +1,7 @@
 """Project entry file."""
 
 # Keep this at beginning of file to prevent circular import with session
-__version__ = "3.5.0"
+__version__ = "3.6.0"
 
 
 from mokkari import exceptions, session, sqlite_cache
